936929c9aff238f354041e1a1b012636ebae2861
Lorenzo Navarro <u201713141@upc.edu.pe>
2023-10-13 19:30:34 -0500

    feat(model) comic/movie added

---
+++ src/models/comic.ts
@@ -0,0 +1,11 @@
+export class Comic {
+  id: number;
+  title: string;
+  description: string;
+
+  constructor() {
+    this.id = 0;
+    this.title = "";
+    this.description = "";
+  }
+}
---
+++ src/models/movie.ts
@@ -0,0 +1,10 @@
+export class Movie {
+  id: number;
+  title: string;
+  description: string;
+  constructor() {
+    this.id = 0;
+    this.title = "";
+    this.description = "";
+  }
+}
